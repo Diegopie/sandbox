5b9f8108c30c9475853a187c1aa9c21560a2f6ef
Diego Hernandez <ishtaracademy@outlook.com>
2022-07-01 18:32:48 -0600

    tried to check for matching values

--- 2022/codeWars/common3and5.js
+++ 2022/codeWars/common3and5.js
@@ -3,23 +3,41 @@ function solution(number) {
     let sum = 0;
 
     for (let i = 1; i < number; i++) {
+        console.log("Loop Number " + i);
+        let three;
+        let five;
+
+         // * Check Multiples of 3
         const divide3 = i / 3;
         // console.log("--");
         // console.log(divide3);
         if (Number.isInteger(divide3)) {
             console.log({i});
-            sum += i
+            three = i;
+            console.log({three});
         }
+        // * Check Multiples of 5
         const divide5 = i / 5;
         // console.log("--");
         // console.log(divide5);
         if (Number.isInteger(divide5)) {
             console.log({i});
-            sum += i
+            five = i;
         }
-        console.log(sum);
+
+        // * Check if 3 and 5 are the same
+        if (three !== five) {
+            if (three) {
+                sum += three;
+            }
+            if (five) {
+                sum += five
+            }
+            
+        }
+        console.log({sum});
     }
     return sum
 }
 
-solution(10);
+solution(168);
